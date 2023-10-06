9bced446b1be882ad22c7026360733865ca92457
CarmoMat <matheushgdocarmo@gmail.com>
2023-10-06 15:28:37 -0300

    Atualização de exercicio 5 e 6

---
+++ exercicio5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+
+    class Desafio5
+    {
+        static void Main()
+        {
+            double peso;
+
+            Console.Write("Digite o peso dos peixes (em Kg): ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out peso))
+            {
+                if (peso < 50.0)
+                {
+                    Console.WriteLine($"A pesca hoje não ultrapassou os valores estabelecidos!");
+                    Console.WriteLine($"Total pescado: {peso} Kg | Total de multa R$ 0");
+                }
+                else
+                {
+                    double excesso = peso - 50.0;
+                    double multa = excesso * 4.0;
+                    Console.WriteLine($"A pesca hoje ultrapassou os valores estabelecidos!");
+                    Console.WriteLine($"Total ultrapassado: {excesso:F2} Kg | Total de multa R$ {multa:F2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Certifique-se de digitar um número válido (use ponto decimal, se necessário).");
+            }
+        }
+    }
---
+++ exercicio6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class exercicio6
+{
+    static void Main()
+    {
+
+Console.Write("Digite a idade do Nadador: ");
+int age = int.Parse(Console.ReadLine());
+
+while (age < 5)
+{
+    Console.WriteLine("Idade mínima é 5!");
+    Console.Write("Digite a idade do Nadador: ");
+    age = int.Parse(Console.ReadLine());
+}
+
+if (age <= 7)
+{
+    Console.WriteLine("Sua categoria é Infantil A!");
+}
+else if (age <= 11)
+{
+    Console.WriteLine("Sua categoria é Infantil B!");
+}
+else if (age <= 13)
+{
+    Console.WriteLine("Sua categoria é Juvenil A!");
+}
+else if (age <= 17)
+{
+    Console.WriteLine("Sua categoria é Juvenil B!");
+}
+else
+{
+    Console.WriteLine("Sua categoria é Adultos!");
+}
+}
+}
