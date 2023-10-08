3e3e2ec49fc2759fa1c6ebb10459410faeefea65
CarmoMat <matheushgdocarmo@gmail.com>
2023-10-08 19:03:52 -0300

    Coding-Tank

---
+++ Exercicio-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+Console.WriteLine("Bem vindo ao conversor de moedas!");
+Console.Write("Digite o valor em Reais ");
+string real = Console.ReadLine();
+Console.WriteLine("Digite para qual moeda deseja converter: ");
+Console.WriteLine("1 - Dólar ");
+Console.WriteLine("2 - Euro ");
+string opcaoStrg = Console.ReadLine();
+
+int num1 = int.Parse(real);
+int opcao = int.Parse(opcaoStrg);
+
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine("Você selecionou a opção 1 - Dólar ");
+        { 
+            double somaDolar = num1 * 5.50;
+            Console.WriteLine($"A opção em Dólar é USD {somaDolar}");
+         }
+        break;
+    case 2:
+        Console.WriteLine("Você selecionou a opção 2 - Euro ");
+        {
+            double somaEuro = num1 * 6.50;
+            Console.WriteLine($"A opção em Euro é EUR {somaEuro}");
+        }
+        break;
+    default:
+        Console.WriteLine("Opção inválida!");
+        break;
+
+
+
+}
+
---
+++ Exercicio-7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class desafio7
+{
+    static void Main()
+    {
+        Console.WriteLine("Iniciando sistemas de vendas...");
+        Console.WriteLine("________________________________");
+        Console.Write("Digite o código do produto: ");
+        int codigoProduto = int.Parse(Console.ReadLine());
+        Console.Write("Digite o nome do produto: ");
+        string nomeProduto = Console.ReadLine();
+        Console.Write("Digite a quantidade comprada: ");
+        int quantidadeComprada = int.Parse(Console.ReadLine());
+        Console.Write("Digite o valor unitário do produto: R$");
+        double valorUnitario = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Carregando dados da venda...");
+        Console.Clear();
+
+        Console.WriteLine("________________________________");
+        Console.WriteLine($"Id: #{codigoProduto}");
+        Console.WriteLine($"Nome do produto: {nomeProduto}");
+        Console.WriteLine($"Quantidade comprada: {quantidadeComprada} Und");
+        Console.WriteLine($"Valor unitário: R${valorUnitario}");
+        Console.WriteLine($"Valor total da venda: R${quantidadeComprada * valorUnitario}");
+        Console.WriteLine("________________________________");
+        Console.WriteLine("Venda Efetuada com sucesso!");
+    }
+}
